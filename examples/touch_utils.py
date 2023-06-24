7935badb63321f327077c8be7d77db834ac2a81b
Shaoxiong Yao <syao16@illinois.edu>
2023-06-24 15:38:52 -0500

    save sim_time_lst

--- examples/touch_utils.py
+++ examples/touch_utils.py
@@ -49,28 +49,44 @@ class TouchSeq:
         if seq_id is None:
             self.seq_dir = self.seq_dir + f'/seq_{int(time.time())}'
             pathlib.Path(self.seq_dir).mkdir(parents=True, exist_ok=True)
+            self.seq_len = 0
+            self.sim_time_lst = []
         else:
             self.seq_dir = self.seq_dir + f'/seq_{seq_id}'
+            self.sim_time_lst = np.load(self.seq_dir + '/sim_time.npy').tolist()
 
             self.q_fn_lst = sorted(glob.glob(self.seq_dir + '/particle_q_*.npy'))
             self.qd_fn_lst = sorted(glob.glob(self.seq_dir + '/particle_qd_*.npy'))
             self.f_fn_lst = sorted(glob.glob(self.seq_dir + '/particle_f_*.npy'))
             self.cid_fn_lst = sorted(glob.glob(self.seq_dir + '/contact_particle_*.npy'))
 
-    def save(self, sim_time, model, state):
+            assert len(self.q_fn_lst) == len(self.qd_fn_lst) == len(self.f_fn_lst) == len(self.cid_fn_lst)
+            self.seq_len = len(self.q_fn_lst)
+    
+    def __len__(self):
+        return self.seq_len
 
+    def save(self, sim_time, model, state):
+        self.sim_time_lst.append(sim_time)
         np.save(self.seq_dir + f'/body_q_{sim_time:07.3f}.npy', state.body_q.numpy())
         np.save(self.seq_dir + f'/particle_q_{sim_time:07.3f}.npy', state.particle_q.numpy())
         np.save(self.seq_dir + f'/particle_qd_{sim_time:07.3f}.npy', state.particle_qd.numpy())
         np.save(self.seq_dir + f'/particle_f_{sim_time:07.3f}.npy', state.particle_f.numpy())
         np.save(self.seq_dir + f'/contact_particle_{sim_time:07.3f}.npy', model.soft_contact_particle.numpy())
+
+        self.seq_len += 1
+    
+    def end_seq(self):
+        np.save(self.seq_dir + f'/sim_time.npy', np.array(self.sim_time_lst))
     
     def load(self, idx):
+        sim_time = self.sim_time_lst[idx]
+
         q = np.load(self.q_fn_lst[idx])
         qd = np.load(self.qd_fn_lst[idx])
         f = np.load(self.f_fn_lst[idx])
         cid = np.load(self.cid_fn_lst[idx])
 
-        return q, qd, f, cid
+        return sim_time, q, qd, f, cid
 
 
