82c97c5204c0432c6165de3bd35c09f25d2c3989
Shaoxiong Yao <syao16@illinois.edu>
2023-06-30 17:17:14 -0500

    random update

--- examples/example_sim_tree.py
+++ examples/example_sim_tree.py
@@ -23,6 +23,7 @@ import open3d as o3d
 
 import warp.sim
 import warp.sim.render
+from warp.sim.integrator_euler import compute_forces
 
 from pxr import Usd, UsdGeom
 
@@ -50,7 +51,7 @@ class Example:
         self.sim_height = 32
 
         self.sim_fps = 60.0
-        self.sim_substeps = 32
+        self.sim_substeps = 128
         self.sim_duration = 10.0
         self.sim_frames = int(self.sim_duration * self.sim_fps)
         self.sim_dt = (1.0 / self.sim_fps) / self.sim_substeps
@@ -58,11 +59,11 @@ class Example:
         # self.sim_use_graph = wp.get_device().is_cuda
         self.sim_use_graph = False
 
-        builder = wp.sim.ModelBuilder(gravity=-0.1)
+        builder = wp.sim.ModelBuilder(gravity=0.0)
         builder.default_particle_radius = 0.0001
 
-        verts = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-        indices = [0, 1, 2, 0, 1, 3, 0, 2, 3, 1, 2, 3]
+        # verts = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 1.0]])
+        # indices = [0, 1, 2, 1, 2, 3]
 
         out_geom = o3d.io.read_triangle_mesh("/home/motion/sparse_tree_tri_mesh.ply")
         out_geom.compute_vertex_normals()
@@ -74,57 +75,53 @@ class Example:
         print("number of vertices:", verts.shape[0])
         indices = np.array(out_geom.triangles).reshape(-1)
 
-        mesh = wp.sim.Mesh(verts, indices)
-
-        # builder.add_shape_mesh(
-        #     body=-1,
-        #     mesh=mesh,
-        #     pos=(1.0, 0.0, 1.0),
+        # builder.add_cloth_mesh(
+        #     pos=(0.0, 0.5, 0.0),
         #     rot=wp.quat_from_axis_angle((1.0, 0.0, 0.0), -math.pi * 0.5),
-        #     scale=(1.0, 1.0, 1.0),
-        #     ke=1.0e2,
-        #     kd=1.0e2,
-        #     kf=1.0e1,
-        # )
-
-        builder.add_cloth_mesh(
-            pos=(0.0, 0.5, 0.0),
-            rot=wp.quat_from_axis_angle((1.0, 0.0, 0.0), -math.pi * 0.5),
-            scale=1.0,
-            vel=(0.0, 0.0, 0.0),
-            vertices=verts,
-            indices=indices,
-            density=1.0
-        )
-
-        # builder.add_cloth_grid(
-        #     pos=(0.0, 10.0, 0.0),
-        #     rot=wp.quat_from_axis_angle((1.0, 0.0, 0.0), math.pi * 0.5),
+        #     scale=1.0,
         #     vel=(0.0, 0.0, 0.0),
-        #     dim_x=16,
-        #     dim_y=8,
-        #     cell_x=0.4,
-        #     cell_y=0.4,
-        #     mass=0.1,
-        #     fix_left=True,
-        #     tri_ke=1.0e3,
-        #     tri_ka=1.0e3,
-        #     tri_kd=1.0e1,
+        #     vertices=verts,
+        #     indices=indices,
+        #     density=1.0,
+        #     tri_ke=100000.0,
+        #     tri_ka=100000.0,
+        #     tri_kd=100000.0,
+        #     edge_ke=100000.0,
+        #     edge_kd=100000.0,
         # )
+        
+        verts *= 1.0
+        verts[:, [1, 2]] = verts[:, [2, 1]]
+
+        for i in range(verts.shape[0]):
+            builder.add_particle(pos=verts[i], vel=(0.0, 0.0, 0.0), mass=1000.0, radius=0.001)
+
+        for tri in np.array(out_geom.triangles):
+            if np.linalg.norm(verts[tri[0]] - verts[tri[1]]) > 1e-3:
+                builder.add_spring(tri[0], tri[1], ke=0.0, kd=1.0, control=0.0)
+            if np.linalg.norm(verts[tri[1]] - verts[tri[2]]) > 1e-3:
+                builder.add_spring(tri[1], tri[2], ke=0.0, kd=1.0, control=0.0)
+            if np.linalg.norm(verts[tri[0]] - verts[tri[2]]) > 1e-3:
+                builder.add_spring(tri[0], tri[2], ke=0.0, kd=1.0, control=0.0)
+            # builder.add_triangle(tri[0], tri[1], tri[2]) 
+                                #  tri_ke=100000.0, tri_ka=100000.0, tri_kd=100000.0)
+
+        b = builder.add_body(origin=wp.transform((-2.0, 1.5, 0.0), wp.quat_identity()), m=0.0)
+        builder.add_shape_sphere(body=b, radius=0.75, density=0.0)
 
         self.model = builder.finalize()
         self.model.ground = True
         self.model.soft_contact_ke = 1.0e4
         self.model.soft_contact_kd = 1.0e2
 
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(self.model.particle_q.numpy())
+        # pcd = o3d.geometry.PointCloud()
+        # pcd.points = o3d.utility.Vector3dVector(self.model.particle_q.numpy())
 
-        tri_mesh = o3d.geometry.TriangleMesh()
-        tri_mesh.vertices = o3d.utility.Vector3dVector(self.model.particle_q.numpy())
-        tri_mesh.triangles = o3d.utility.Vector3iVector(self.model.tri_indices.numpy())
+        # tri_mesh = o3d.geometry.TriangleMesh()
+        # tri_mesh.vertices = o3d.utility.Vector3dVector(self.model.particle_q.numpy())
+        # tri_mesh.triangles = o3d.utility.Vector3iVector(self.model.tri_indices.numpy())
 
-        o3d.visualization.draw_geometries([pcd, tri_mesh])
+        # o3d.visualization.draw_geometries([pcd, tri_mesh])
 
         # self.integrator = wp.sim.SemiImplicitIntegrator()
         self.integrator = wp.sim.XPBDIntegrator()
@@ -132,50 +129,41 @@ class Example:
         self.state_0 = self.model.state()
         self.state_1 = self.model.state()
 
+        compute_forces(self.model, self.state_0, self.state_0.particle_f, self.state_0.body_f, False)
+        np.save("particle_f.npy", self.state_0.particle_f.numpy())
+        print("particle f:", self.state_0.particle_f.numpy())
+        print("body f:", self.state_0.body_f.numpy())
+        input()
+
         print("init particle q:", self.state_0.particle_q.numpy())
 
         self.renderer = wp.sim.render.SimRendererOpenGL(self.model, stage, scaling=1.0)
         # self.renderer = wp.sim.render.SimRenderer(self.model, stage, scaling=40.0)
 
-        if self.sim_use_graph:
-            # create update graph
-            wp.capture_begin()
+    def update(self):
+        with wp.ScopedTimer("simulate", active=True):
+            print("INFO: update")
+            # if self.sim_time <= 10.0:
+            #     self.state_0.body_q.assign(
+            #         [[-2.0 + self.sim_time/5.0, 1.5, 0.0, 0., 0., 0., 1.]]
+            #     )
 
             wp.sim.collide(self.model, self.state_0)
 
             for s in range(self.sim_substeps):
-                print("s:", s)
                 self.state_0.clear_forces()
 
                 self.integrator.simulate(self.model, self.state_0, self.state_1, self.sim_dt)
 
+                # self.clamp_state(self.state_0, self.state_1)
+
+                print("state diff:", np.linalg.norm(self.state_0.particle_q.numpy() - self.state_1.particle_q.numpy()))
+
                 # swap states
                 (self.state_0, self.state_1) = (self.state_1, self.state_0)
-
-            self.graph = wp.capture_end()
-
-    def update(self):
-        with wp.ScopedTimer("simulate", active=True):
-            if self.sim_use_graph:
-                wp.capture_launch(self.graph)
-            else:
-                print("INFO: update")
-                wp.sim.collide(self.model, self.state_0)
-
-                for s in range(self.sim_substeps):
-                    self.state_0.clear_forces()
-
-                    self.integrator.simulate(self.model, self.state_0, self.state_1, self.sim_dt)
-
-                    # self.clamp_state(self.state_0, self.state_1)
-
-                    print("state diff:", np.linalg.norm(self.state_0.particle_q.numpy() - self.state_1.particle_q.numpy()))
-
-                    # swap states
-                    (self.state_0, self.state_1) = (self.state_1, self.state_0)
-                
-                print("new state:", self.state_0.particle_q.numpy())
-                # input()
+            
+            print("new state:", self.state_0.particle_q.numpy())
+            # input()
 
     def clamp_state(self, state0, state1):
         wp.launch(
@@ -198,6 +186,10 @@ class Example:
 
             self.renderer.begin_frame(time)
             self.renderer.render(self.state_0)
+            # body_q = self.state_0.body_q.numpy()
+            # self.renderer.render_line_list()
+            # self.renderer.render_sphere(name='sphere', pos=body_q[:3], rot=body_q[3:], radius=0.75)
+            # self.renderer.render_points(self, points=self.state_0.particle_q.numpy(), radius=0.01)
             self.renderer.end_frame()
 
         self.sim_time += 1.0 / self.sim_fps
@@ -210,6 +202,7 @@ if __name__ == "__main__":
 
     for i in range(example.sim_frames):
         example.update()
+        print(f"step {i}: update")
 
         if np.isnan(example.state_0.particle_q.numpy()).any():
             break
