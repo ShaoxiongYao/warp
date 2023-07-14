52f32f4dff97298d6aa416e066cec42d9ddc096c
ShaoxiongYao <syao16@illinois.edu>
2023-07-14 08:32:33 +0800

    run convert_tet_file on dgn_dataset

--- examples/convert_tet_file.py
+++ examples/convert_tet_file.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glob
 import meshio
+import os
 
 def extract_tet_mesh(in_fn):
     pts_lst = []
@@ -24,15 +25,27 @@ def extract_tet_mesh(in_fn):
 
 if __name__ == '__main__':
 
+    fail_obj_lst = []
+
     obj_dir_lst = glob.glob('/home/yaosx/Downloads/dgn_dataset/*')
     for obj_dir in obj_dir_lst:
-        print("obj_dir:", obj_dir)
+        obj_name = obj_dir.split('/')[-1]
+        print("obj_name:", obj_name)
 
+        in_fn = obj_dir + '/' + obj_name + '.tet'
+        out_fn = obj_dir + '/' + obj_name + '.msh'
 
-        # pts_lst, tet_lst = extract_tet_mesh(in_fn)
+        print("in_fn:", in_fn)
+        print("out_fn:", out_fn)
+        
+        if os.path.exists(in_fn):
+            pts_lst, tet_lst = extract_tet_mesh(in_fn)
 
-        # cells = [
-        #     ('tetra', tet_lst)
-        # ]
+            cells = [ ('tetra', tet_lst) ]
 
-        # meshio.write_points_cells(out_fn, pts_lst, cells)
+            meshio.write_points_cells(out_fn, pts_lst, cells)
+        else:
+            print(f"INFO: object {obj_name} has no .tet file")
+            fail_obj_lst.append(obj_name)
+    
+    print("failed obj lst:", fail_obj_lst)
